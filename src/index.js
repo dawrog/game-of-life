167c07aa97979ab3c400df3c10b550a43330e4a4
dawrog <dawid.rogal@protonmail.com>
2018-03-30 00:00:37 +0200

    added main app component and orginized file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Main extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+                <Grid 
+                />
+                <h2>Generations: {this.state.generation}</h2>
+            </div>
+        );
+    }
+
+
+}
+
+ReactDOM.render(<p>Hello World!</p>, document.getElementById('root'));
+
