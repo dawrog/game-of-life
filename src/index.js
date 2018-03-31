f5bcf73e5d10d4bf2640aca0951a9ce0f4d3aa88
dawrog <dawid.rogal@protonmail.com>
2018-03-31 23:59:52 +0200

    added grid component to main app

--- src/index.js
+++ src/index.js
@@ -2,21 +2,45 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Main extends React.Component {
+class Grid extends React.Component {
+	render() {
+		return (
+			<div>
+				Grid
+			</div>
+		);
+	}
+}
 
-    render() {
-        return (
-            <div>
-                <h1>The Game of Life</h1>
-                <Grid 
-                />
-                <h2>Generations: {this.state.generation}</h2>
-            </div>
-        );
-    }
+class Main extends React.Component {
+	constructor() {
+		super();
+		this.speed = 100;
+		this.rows = 30;
+		this.cols = 50;
+
+		this.state = {
+			generation: 0,
+			gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>The Game of Life</h1>
+				<Grid
+					gridFull={this.state.gridFull}
+					rows={this.rows}
+					cols={this.cols}
+				/>
+				<h2>Generations: {this.state.generation}</h2>
+			</div>
+		);
+	}
 
 
 }
 
-ReactDOM.render(<p>Hello World!</p>, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 
