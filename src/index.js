ec0b9ba6760bc7c3301395f2fb4a96b2929f6caf
dawrog <dawid.rogal@protonmail.com>
2018-04-03 23:59:07 +0200

    added buttons with react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
 
 class Box extends React.Component {
 	selectBox = () => {
@@ -50,6 +51,52 @@ class Grid extends React.Component {
 	}
 }
 
+class Buttons extends React.component {
+
+	handleSelect = (evt) => {
+		this.props.gridSize(evt);
+	}
+}
+
+class Buttons extends React.component {
+
+	render() {
+		return (
+			<div className="center">
+				<ButtonToolbar>
+					<button className="btn btn-default" onClick={this.props.playButton}>
+						Play
+					</button>
+					<button className="btn btn-default" onClick={this.props.pauseButton}>
+						Pause
+					</button>
+					<button className="btn btn-default" onClick={this.props.clear}>
+						Clear
+					</button>
+					<button className="btn btn-default" onClick={this.props.slow}>
+						Slow
+					</button>
+					<button className="btn btn-default" onClick={this.props.fast}>
+						Fast
+					</button>
+					<button className="btn btn-default" onClick={this.props.seed}>
+						Seed
+					</button>
+					<DropdownButton
+						title="Grid Size"
+						id="size-menu"
+						onSelect={this.handleSelect}
+					>
+						<MenuItem eventKey="1">20x10</MenuItem>
+						<MenuItem eventKey="2">50x30</MenuItem>
+						<MenuItem eventKey="3">70x50</MenuItem>
+					</DropdownButton>
+				</ButtonToolbar>
+			</div>
+		);
+	}
+}
+
 class Main extends React.Component {
 	constructor() {
 		super();
@@ -87,8 +134,12 @@ class Main extends React.Component {
 	}
 
 	playButton = () => {
-		clearInterval(this.intervalId)
+		clearInterval(this.intervalId);
 		this.intervalId = setInterval(this.play, this.speed);
+	}
+
+	pauseButton = () => {
+		clearInterval(this.intervalId);
 	}
 
 	play = () => {
@@ -125,6 +176,15 @@ class Main extends React.Component {
 		return (
 			<div>
 				<h1>The Game of Life</h1>
+				<Buttons
+					playButton={this.playButton}
+					pauseButton={this.pauseButton}
+					slow={this.slow}
+					fast={this.fast}
+					clear={this.clear}
+					seed={this.seed}
+					gridSize={this.gridSize}
+				/>
 				<Grid
 					gridFull={this.state.gridFull}
 					rows={this.rows}
